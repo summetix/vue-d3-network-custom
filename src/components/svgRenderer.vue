9a557d0469b6e5772beb5342a3d4371bcf50f425
g-jih <rhakdnj47@gmail.com>
2021-01-15 17:39:56 +0100

    link-label in link group

--- src/components/svgRenderer.vue
+++ src/components/svgRenderer.vue
@@ -24,6 +24,7 @@
           :class='linkClass(link.id)'
           :style='linkStyle(link)'
           )
+        textPath(class='hoverlabel' v-bind:xlink:href="'#' + link.id" startOffset= "50%") {{ link.name }}
 
     //- -> nodes
     g.nodes#l-nodes(v-if='!noNodes')
