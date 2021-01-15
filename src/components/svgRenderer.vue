15ebaaee52b43b49b6cd9edef5ed6be0b86b08e9
g-jih <rhakdnj47@gmail.com>
2021-01-15 17:45:01 +0100

    error solved

--- src/components/svgRenderer.vue
+++ src/components/svgRenderer.vue
@@ -24,7 +24,7 @@
           :class='linkClass(link.id)'
           :style='linkStyle(link)'
           )
-        textPath(class='hoverlabel' v-bind:xlink:href="'#' + link.id" startOffset= "50%") {{ link.name }}
+          textPath(:class='hoverlabel' v-bind:xlink:href="'#' + link.id" startOffset= "50%") {{ link.name }}
 
     //- -> nodes
     g.nodes#l-nodes(v-if='!noNodes')
